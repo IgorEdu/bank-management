0e8916a98b8e569e352faa9f1f4988c976805970
Igor <igoreduardo20101@gmail.com>
2025-03-28 15:12:00 -0300

    feat(Account): create accountRepository

---
+++ src/main/java/com/ng_billing/bank_management/repository/AccountRepository.java
@@ -0,0 +1,11 @@
+package com.ng_billing.bank_management.repository;
+
+import com.ng_billing.bank_management.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AccountRepository extends JpaRepository<Account, UUID> {
+
+    Account findByAccountNumber(int accountNumber);
+}
