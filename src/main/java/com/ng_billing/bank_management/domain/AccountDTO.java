6cb19e471d977237dd31281b6e6e9555009c952c
Igor <igoreduardo20101@gmail.com>
2025-03-28 16:28:30 -0300

    refactor: rename AccountResponseDTO to AccountDTO for consistency

---
+++ src/main/java/com/ng_billing/bank_management/domain/AccountDTO.java
@@ -0,0 +1,4 @@
+package com.ng_billing.bank_management.domain;
+
+public record AccountDTO(int accountNumber, float balance) {
+}
--- src/main/java/com/ng_billing/bank_management/domain/AccountResponseDTO.java
+++
@@ -1,4 +0,0 @@
-package com.ng_billing.bank_management.domain;
-
-public record AccountResponseDTO(int accountNumber, float balance) {
-}
