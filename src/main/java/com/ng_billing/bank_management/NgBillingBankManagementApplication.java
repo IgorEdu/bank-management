8856ff68519ae17e429468d59c744e566a5c84c7
Igor <igoreduardo20101@gmail.com>
2025-03-28 14:02:02 -0300

    Initial commit

---
+++ src/main/java/com/ng_billing/bank_management/NgBillingBankManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ng_billing.bank_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgBillingBankManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgBillingBankManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ng_billing/bank_management/NgBillingBankManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ng_billing.bank_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgBillingBankManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
