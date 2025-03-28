470cac75d1f39b1503a6431ed8232c4bc3aba840
Igor <igoreduardo20101@gmail.com>
2025-03-28 16:30:36 -0300

    refactor: change return type of findByAccountNumber to Optional<Account>

--- src/main/java/com/ng_billing/bank_management/repository/AccountRepository.java
+++ src/main/java/com/ng_billing/bank_management/repository/AccountRepository.java
@@ -3,9 +3,10 @@ package com.ng_billing.bank_management.repository;
 import com.ng_billing.bank_management.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AccountRepository extends JpaRepository<Account, UUID> {
 
-    Account findByAccountNumber(int accountNumber);
+    Optional<Account> findByAccountNumber(int accountNumber);
 }
