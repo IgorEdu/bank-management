12f3b01ca1c353b0474c389e7c0d2a61b3eac8c8
Igor <igoreduardo20101@gmail.com>
2025-03-29 14:43:17 -0300

    feat(dto): create TransactionDTO and add validation messages for TransactionDTO fields

---
+++ src/main/java/com/ng_billing/bank_management/domain/TransactionDTO.java
@@ -0,0 +1,21 @@
+package com.ng_billing.bank_management.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+
+public record TransactionDTO(
+        @NotNull(message = "Forma de pagamento não pode ser nulo")
+        TransactionType type,
+
+        @NotNull(message = "Número da conta não pode ser nulo")
+        @JsonProperty("numero_conta")
+        int accountNumber,
+
+        @NotNull(message = "Valor não pode ser nulo")
+        @Positive(message = "Valor deve ser positivo")
+        @JsonProperty("valor")
+        BigDecimal value) {
+}
