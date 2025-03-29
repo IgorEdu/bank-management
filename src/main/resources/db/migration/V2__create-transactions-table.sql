4b4d289fbf8596055f905748168400e3947bc2fc
Igor <igoreduardo20101@gmail.com>
2025-03-28 23:16:07 -0300

    feat(migration): create transactions table with trigger for updated_at
    - Added `transactions` table to store transaction data, including fields for `id`, `account`, `type`, `value`, `created_at`, and `updated_at`.
    - Defined a `CHECK` constraint on the `type` column to ensure valid transaction types ('P', 'C', 'D').
    - Created trigger `update_transaction_updated_at` to automatically update the `updated_at` column before each record update.

---
+++ src/main/resources/db/migration/V2__create-transactions-table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE transactions (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    account UUID NOT NULL,
+    type CHAR(1) NOT NULL CHECK (type IN ('P', 'C', 'D')),
+    value DECIMAL(15,2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TRIGGER update_transaction_updated_at
+BEFORE UPDATE ON transactions
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at();
