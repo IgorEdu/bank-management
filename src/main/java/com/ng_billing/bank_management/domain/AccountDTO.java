286fb4283f65914ea359cfa2f7aba0e38c1dfca4
Igor <igoreduardo20101@gmail.com>
2025-03-28 22:52:52 -0300

    refactor(AccountDTO): update DTO with validation and custom messages
    - Changes the `AccountDTO` class to use `Integer` and `Float` types instead of primitive types.
    - Adds custom validation messages for `accountNumber` and `balance` fields.
    - Applies `@JsonProperty` annotations to ensure proper JSON field naming (`numero_conta` and `saldo`).

--- src/main/java/com/ng_billing/bank_management/domain/AccountDTO.java
+++ src/main/java/com/ng_billing/bank_management/domain/AccountDTO.java
@@ -1,6 +1,17 @@
 package com.ng_billing.bank_management.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
-public record AccountDTO(@NotNull int accountNumber,@NotNull float balance) {
+public record AccountDTO(
+        @NotNull(message = "Número da conta não pode ser nulo")
+        @JsonProperty("numero_conta")
+        Integer accountNumber,
+
+        @NotNull(message = "Saldo não pode ser nulo")
+        @Positive(message = "Saldo deve ser positivo")
+        @JsonProperty("saldo")
+        Float balance
+) {
 }
