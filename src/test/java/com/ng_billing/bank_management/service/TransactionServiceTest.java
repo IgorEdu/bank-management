70f7a751b6cfdaff8d314c3baf16d4e184fc41be
Igor <igoreduardo20101@gmail.com>
2025-03-29 13:27:32 -0300

    test(TransactionServiceTest): add tests for insufficient balance and account not found scenarios
    - Added tests for insufficient balance scenarios:
      - Pix transaction: throws exception when the account balance is insufficient (balance < transaction value).
      - Debit transaction: throws exception when the account balance is insufficient (balance < transaction value + fee).
      - Credit transaction: throws exception when the account balance is insufficient (balance < transaction value + fee).
    
    - Added tests for account not found scenarios:
      - Throws `AccountNotFoundException` when the account does not exist for Pix, Debit, or Credit transactions.
    
    - Refactored existing tests to use BigDecimal instead of float for transaction amounts and balances.
    - Adjusted assertions and mock setups to support the new BigDecimal type and ensure accuracy in financial calculations.

--- src/test/java/com/ng_billing/bank_management/service/TransactionServiceTest.java
+++ src/test/java/com/ng_billing/bank_management/service/TransactionServiceTest.java
@@ -14,6 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.security.auth.login.AccountNotFoundException;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -42,13 +43,13 @@ class TransactionServiceTest {
     @Test
     @DisplayName("Deve realizar a transação de débito com sucesso quando a conta tiver saldo suficiente")
     void shouldCompleteDebitTransactionWhenAccountHasSufficientBalance() throws AccountNotFoundException, InsufficientBalanceException {
-        float transactionValue = 100.00f;
-        float fee = transactionValue * 0.03f;
-        float totalAmount = transactionValue + fee;
+        BigDecimal transactionValue = BigDecimal.valueOf(100.00);
+        BigDecimal fee = transactionValue.multiply(BigDecimal.valueOf(0.03));
+        BigDecimal totalAmount = transactionValue.add(fee);
 
         Transaction transaction = new Transaction(account, TransactionType.D, transactionValue);
 
-        when(account.getBalance()).thenReturn(103.00f);
+        when(account.getBalance()).thenReturn(BigDecimal.valueOf(103));
         when(accountService.getAccountByNumber(anyInt())).thenReturn(Optional.of(account));
         when(transactionRepository.save(any(Transaction.class))).thenReturn(transaction);
 
@@ -58,17 +59,20 @@ class TransactionServiceTest {
         assertNotNull(result);
         assertEquals(transactionValue, result.getValue());
         assertEquals(TransactionType.D, result.getType());
-        assertEquals(totalAmount, account.getBalance(), 0.01f);
+
+        verify(accountService).decreaseBalance(account, totalAmount);
     }
 
     @Test
     @DisplayName("Deve realizar a transação de crédito com sucesso quando a conta tiver saldo suficiente")
     void shouldCompleteCreditTransactionWhenAccountHasSufficientBalance() throws AccountNotFoundException, InsufficientBalanceException {
-        float transactionValue = 100.00f;
-        float fee = transactionValue * 0.05f;
-        float totalAmount = transactionValue + fee;
+        BigDecimal transactionValue = BigDecimal.valueOf(100);
+        BigDecimal fee = transactionValue.multiply(BigDecimal.valueOf(0.05));
+        BigDecimal totalAmount = transactionValue.add(fee);
 
-        when(account.getBalance()).thenReturn(1000.00f);
+        Transaction transaction = new Transaction(account, TransactionType.C, transactionValue);
+
+        when(account.getBalance()).thenReturn(BigDecimal.valueOf(105));
         when(accountService.getAccountByNumber(anyInt())).thenReturn(Optional.of(account));
         when(transactionRepository.save(any(Transaction.class))).thenReturn(transaction);
 
@@ -78,16 +82,18 @@ class TransactionServiceTest {
         assertNotNull(result);
         assertEquals(transactionValue, result.getValue());
         assertEquals(TransactionType.C, result.getType());
-        assertEquals(totalAmount, account.getBalance(), 0.01f);
+
+        verify(accountService).decreaseBalance(account, totalAmount);
     }
 
     @Test
     @DisplayName("Deve realizar a transação de Pix com sucesso quando a conta tiver saldo suficiente")
     void shouldCompletePixTransactionWhenAccountHasSufficientBalance() throws AccountNotFoundException, InsufficientBalanceException {
-        float transactionValue = 100.00f;
-        float totalAmount = transactionValue;
+        BigDecimal transactionValue = BigDecimal.valueOf(100);
 
-        when(account.getBalance()).thenReturn(1000.00f);
+        Transaction transaction = new Transaction(account, TransactionType.P, transactionValue);
+
+        when(account.getBalance()).thenReturn(BigDecimal.valueOf(100));
         when(accountService.getAccountByNumber(anyInt())).thenReturn(Optional.of(account));
         when(transactionRepository.save(any(Transaction.class))).thenReturn(transaction);
 
@@ -97,14 +103,49 @@ class TransactionServiceTest {
         assertNotNull(result);
         assertEquals(transactionValue, result.getValue());
         assertEquals(TransactionType.P, result.getType());
-        assertEquals(totalAmount, account.getBalance(), 0.01f);
+
+        verify(accountService).decreaseBalance(account, transactionValue);
     }
 
     @Test
-    @DisplayName("Deve lançar exceção quando a conta não tiver saldo suficiente")
-    void shouldThrowExceptionWhenAccountHasInsufficientBalance() {
+    @DisplayName("Deve lançar exceção quando a conta não tiver saldo suficiente para transação tipo Pix")
+    void shouldThrowExceptionWhenAccountHasInsufficientBalancePixTransaction() {
+        BigDecimal transactionValue = BigDecimal.valueOf(100);
+
+        Transaction transaction = new Transaction(account, TransactionType.P, transactionValue);
+
         when(accountService.getAccountByNumber(anyInt())).thenReturn(Optional.of(account));
-        when(account.getBalance()).thenReturn(100.00f);
+        when(account.getBalance()).thenReturn(BigDecimal.valueOf(99.99));
+
+        assertThrows(InsufficientBalanceException.class, () -> {
+            transactionService.processTransaction(transaction);
+        });
+    }
+
+    @Test
+    @DisplayName("Deve lançar exceção quando a conta não tiver saldo suficiente para transferência tipo Débito")
+    void shouldThrowExceptionWhenAccountHasInsufficientBalanceDebitTransaction() {
+        BigDecimal transactionValue = BigDecimal.valueOf(100);
+
+        Transaction transaction = new Transaction(account, TransactionType.D, transactionValue);
+
+        when(accountService.getAccountByNumber(anyInt())).thenReturn(Optional.of(account));
+        when(account.getBalance()).thenReturn(BigDecimal.valueOf(102.99));
+
+        assertThrows(InsufficientBalanceException.class, () -> {
+            transactionService.processTransaction(transaction);
+        });
+    }
+
+    @Test
+    @DisplayName("Deve lançar exceção quando a conta não tiver saldo suficiente para transferência tipo Crédito")
+    void shouldThrowExceptionWhenAccountHasInsufficientBalanceCreditTransaction() {
+        BigDecimal transactionValue = BigDecimal.valueOf(100);
+
+        Transaction transaction = new Transaction(account, TransactionType.C, transactionValue);
+
+        when(accountService.getAccountByNumber(anyInt())).thenReturn(Optional.of(account));
+        when(account.getBalance()).thenReturn(BigDecimal.valueOf(104.99));
 
         assertThrows(InsufficientBalanceException.class, () -> {
             transactionService.processTransaction(transaction);
@@ -114,6 +155,10 @@ class TransactionServiceTest {
     @Test
     @DisplayName("Deve lançar exceção quando a conta não existir")
     void shouldThrowExceptionWhenAccountDoesNotExist() {
+        BigDecimal transactionValue = BigDecimal.valueOf(100);
+
+        Transaction transaction = new Transaction(account, TransactionType.P, transactionValue);
+
         when(account.getAccountNumber()).thenReturn(234);
         when(accountService.getAccountByNumber(234)).thenReturn(Optional.empty());
 
