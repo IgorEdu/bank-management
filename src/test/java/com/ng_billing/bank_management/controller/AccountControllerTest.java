f9e08f7ae5658c8f4a2e54fef98677798ca5dad2
Igor <igoreduardo20101@gmail.com>
2025-03-28 22:30:49 -0300

    refactor(AccountControllerTest): improve test cases for GET and POST endpoints
    - Updated `testGetAccount_ReturnsAccount` to assert correct response when account is found.
    - Improved `testGetAccount_ReturnsNotFound_WhenAccountDoesNotExist` to verify correct response when account does not exist.
    - Added test for `createAccount` with successful creation (`testCreateAccount_ReturnsCreated`).
    - Added test for `createAccount` with existing account exception (`testCreateAccount_ReturnsBadRequest_WhenAccountAlreadyExists`).
    - Switched to use `@MockBean` for `AccountService` in integration tests with `MockMvc`.

--- src/test/java/com/ng_billing/bank_management/controller/AccountControllerTest.java
+++ src/test/java/com/ng_billing/bank_management/controller/AccountControllerTest.java
@@ -1,57 +1,100 @@
 package com.ng_billing.bank_management.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ng_billing.bank_management.domain.Account;
 import com.ng_billing.bank_management.domain.AccountDTO;
+import com.ng_billing.bank_management.exceptions.AccountAlreadyExistsException;
 import com.ng_billing.bank_management.service.AccountService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Optional;
 
 import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class AccountControllerTest {
 
     @InjectMocks
     private AccountController accountController;
 
-    @Mock
+    @MockBean
     private AccountService accountService;
 
+    @Autowired
+    private MockMvc mockMvc;
+
     private Account account;
+    private AccountDTO accountDTO;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
 
         account = new Account(234, 170.07f);
+        accountDTO = new AccountDTO(account.getAccountNumber(), account.getBalance());
     }
 
     @Test
-    void testGetAccount_ReturnsAccount() {
+    void testGetAccount_ReturnsAccount() throws Exception {
         when(accountService.getAccountByNumber(234)).thenReturn(Optional.of(account));
 
-        ResponseEntity<AccountDTO> response = accountController.getAccount(234);
+        mockMvc.perform(get("/conta?numero_conta={accountNumber}", 234))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accountNumber").value(234))
+                .andExpect(jsonPath("$.balance").value(170.07));
 
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertNotNull(response.getBody());
-        assertEquals(234, response.getBody().accountNumber());
-        assertEquals(170.07f, response.getBody().balance());
+        verify(accountService, times(1)).getAccountByNumber(234);
     }
 
     @Test
-    void testGetAccount_ReturnsNotFound_WhenAccountDoesNotExist() {
+    void testGetAccount_ReturnsNotFound_WhenAccountDoesNotExist() throws Exception {
         when(accountService.getAccountByNumber(234)).thenReturn(Optional.empty());
 
-        ResponseEntity<AccountDTO> response = accountController.getAccount(234);
+        mockMvc.perform(get("/conta?numero_conta={accountNumber}", 234))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string(""));
 
-        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
-        assertNull(response.getBody());
+        verify(accountService, times(1)).getAccountByNumber(234);
+    }
+
+
+    @Test
+    void testCreateAccount_ReturnsCreated() throws Exception {
+        when(accountService.createAccount(any())).thenReturn(accountDTO);
+
+        mockMvc.perform(post("/conta")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(accountDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.accountNumber").value(234))
+                .andExpect(jsonPath("$.balance").value(170.07));
+
+        verify(accountService, times(1)).createAccount(any());
+    }
+
+
+    @Test
+    void testCreateAccount_ReturnsBadRequest_WhenAccountAlreadyExists() throws Exception {
+        when(accountService.createAccount(any(Account.class)))
+                .thenThrow(new AccountAlreadyExistsException("Conta de número 234 já existente."));
+
+        mockMvc.perform(post("/conta")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(accountDTO)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("Conta de número 234 já existente."));
     }
 }
