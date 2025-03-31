95dea15531258ec6fcc0090dacbe20b011e67397
Igor <igoreduardo20101@gmail.com>
2025-03-31 08:54:15 -0300

    chore: move production migration package to 'postgres' folder

--- src/main/resources/db/migration/postgres/V2__create-transactions-table.sql
+++ src/main/resources/db/migration/postgres/V2__create-transactions-table.sql
@@ -2,7 +2,7 @@ CREATE TABLE transactions (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     account_id UUID NOT NULL,
     type CHAR(1) NOT NULL CHECK (type IN ('P', 'C', 'D')),
-    value DECIMAL(15,2) NOT NULL,
+    amount DECIMAL(15,2) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
